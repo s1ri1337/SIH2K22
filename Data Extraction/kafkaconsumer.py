3b03a077c159e7de189c931086a79df2c5de0e8d
Tanay Gupta <74172739+s1ri1337@users.noreply.github.com>
2022-08-25 10:55:37 +0530

    Initial Commit

---
+++ Data Extraction/kafkaconsumer.py
@@ -0,0 +1,17 @@
+from kafka import KafkaConsumer
+import json
+
+topic_name = 'earthquake'
+
+consumer = KafkaConsumer(
+    topic_name,
+     bootstrap_servers=['localhost:9092'],
+     auto_offset_reset='latest',
+     enable_auto_commit=True,
+     auto_commit_interval_ms=5000,
+     fetch_max_bytes=128,
+     max_poll_records=100,
+     value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+
+for message in consumer:
+    print(json.dumps(message.value, indent=2))
---
+++ Data Extraction/kafkaproducer.py
@@ -0,0 +1,44 @@
+import tweepy
+from kafka import KafkaProducer
+import logging
+
+"""API ACCESS KEYS"""
+
+consumerKey = "***"
+consumerSecret = "***"
+accessToken = "***"
+accessTokenSecret = "***"
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+search_term = 'earthquake'
+topic_name = 'earthquake'
+
+
+def twitterAuth():
+    # create the authentication object
+    authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret)
+    # set the access token and the access token secret
+    authenticate.set_access_token(accessToken, accessTokenSecret)
+    # create the API object
+    api = tweepy.API(authenticate, wait_on_rate_limit=True)
+    return api
+
+
+class TweetListener(tweepy.Stream):
+
+    def on_data(self, raw_data):
+        logging.info(raw_data)
+        producer.send(topic_name, value=raw_data)
+        return True
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            # returning False in on_data disconnects the stream
+            return False
+
+    def start_streaming_tweets(self, search_term):
+        self.filter(track=search_term, stall_warnings=True, languages=["en"])
+
+if __name__ == '__main__':
+    twitter_stream = TweetListener(consumerKey, consumerSecret, accessToken, accessTokenSecret)
+    twitter_stream.start_streaming_tweets(search_term)
